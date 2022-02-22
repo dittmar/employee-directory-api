c19e153252c46ab1b9d555240b1f9fccacb04b11
dittmar <kevinhd4@gmail.com>
2022-02-21 19:11:43 -0500

    Documentation and handling TODOs

--- Sources/EmployeeDirectoryApi/Endpoints/Endpoint.swift
+++ Sources/EmployeeDirectoryApi/Endpoints/Endpoint.swift
@@ -26,8 +26,8 @@ public protocol Endpoint {
   var path: String { get }
   
   /// Invoke this `Endpoint` and return its response decoded into the  expected return type
-  ///  Parameter urlSession: the `URLSession` to use for this endpoint call.
-  ///  Returns: the response from the server for the invoked endpoint
+  ///  - Parameter urlSession: the `URLSession` to use for this endpoint call.
+  ///  - Returns: the response from the server for the invoked endpoint
   func invoke(urlSession: URLSession) async throws -> Response?
 }
 
@@ -36,11 +36,11 @@ extension Endpoint {
   static var awsBaseUrl: String { "https://s3.amazonaws.com/sq-mobile-interview" }
   
   /// Invoke this `Endpoint` and return its response decoded into the  expected return type
-  ///  Parameter urlSession: the `URLSession` to use for this endpoint call.  Defaults to `.shared`
+  /// - Parameter urlSession: the `URLSession` to use for this endpoint call.  Defaults to `.shared`
+  /// - Throws: Throws an error with code -1 if the URL derived from the endpoint is malformed
   public func invoke(urlSession: URLSession = .shared) async throws -> Response? {
     guard let url = URL(string: path) else {
-      // TODO (dittmar): throw malformed URL error instead
-      return nil
+      throw NSError(domain: "com.dittmar.Employee-Directory-API", code: -1, userInfo: ["message": "Invalid endpoint URL"])
     }
     
     let (data, _) = try await urlSession.data(from: url, delegate: nil)
--- Tests/EmployeeDirectoryApiTests/TestUtils.swift
+++ Tests/EmployeeDirectoryApiTests/TestUtils.swift
@@ -9,6 +9,11 @@ import Foundation
 import XCTest
 
 extension XCTestCase {
+  /**
+   Read the given JSON file in as `Data` to use for testing purposes
+   - Parameter filename: the name of the JSON file to load
+   - Returns: the JSON contents of the file as `Data` to be consumed by a unit test
+   */
   func readJson(from filename: String) throws -> Data? {
     guard let path = Bundle.module.path(forResource: filename, ofType: "json") else {
       throw NSError(domain: "ResourceError", code: -1, userInfo: ["message": "JSON file \(filename).json not found"])
