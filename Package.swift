0278c71077f296e80de6dd9d79bffed287fdc230
dittmar <kevinhd4@gmail.com>
2022-02-21 22:54:56 -0500

    Fix tests through actions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "EmployeeDirectoryApi",
-    platforms: [.iOS(.v15)],
+    // macOS version is specified to make GitHub Actions happy to run the tests.
+    // Otherwise, it fails, even though they pass locally.
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
